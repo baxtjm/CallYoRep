29ff110a128acad44817301d1c86b8584b5bb0e9
Lewis Jones <lewismjones@gmail.com>
2017-04-13 00:22:13 -0400

    woo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/testrep" =>'representatives#rep_lookup'
+  get "/testrep" =>'representatives#get_response'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

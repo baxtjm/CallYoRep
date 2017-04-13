a397271789be2660edb28f32e1edbed14b11e2e1
Lewis Jones <lewismjones@gmail.com>
2017-04-12 21:50:15 -0400

    playing with views

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,30 +1,19 @@
-#
 class RepresentativesController < ApplicationController
   # before_action :set_representative, only: [:show, :edit, :update, :destroy]
 #
 #
 #
-#   def show
-#         url = "http://whoismyrepresentative.com/getall_mems.php?zip=33133&output=json"
-#
-#         response = HTTParty.get url
-#
-#         puts response #.headers['content-type']
-#
-#         puts response.body
-#
-#         dom = Nokogiri::HTML(response.body)
-#
-#         dom.css("a.hdrlnk").map do |element|
-#           #   p element.content
-#     end
-#   end
 def rep_lookup
   zip = 33133
   @response = HTTParty.get("http://whoismyrepresentative.com/getall_mems.php?zip=#{zip}&output=json", format: :plain)
 
   @reps = JSON.parse @response, symbolize_names: true
-
+   @reps = @reps[:results]
+    @prep =[]
+    @reps.each do |rep|
+      @prep << [rep[:name], rep[:party], rep[:state], rep[:phone]]
+    end
+    puts @prep
 end
 
 

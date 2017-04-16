e652c05d5c093dad634413aef058f3b1f33e2091
Lewis Jones <lewismjones@gmail.com>
2017-04-16 16:52:56 -0400

    updating code to add User model entry

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,7 +1,7 @@
 class TwilioController < ApplicationController
   include Webhookable
 
-  before_action :set_phone, :set_message
+  before_action :set_phone, :set_message, :set_zip, :set_email, :set_rep
   after_action :set_header
   skip_before_action :verify_authenticity_token
 
@@ -13,21 +13,46 @@ def phone_call
             # set up a client to talk to the Twilio REST API
       @client = Twilio::REST::Client.new('AC6c6cc3bf0fa71451edb86c7764f07f5c', 'ce0874767990e1640ad63ae98a4c45e9')
       @call = @client.account.calls.create(
-            :to => "#{@rep}",
+            :to => "#{@phone}",
             :from => "+18573133666",
             :url => Message.find_by_position(@position).audio_link)
             puts @call.to
+
+    user = User.create(zip: @zip, email: @email, message_id: Message.find_by_position(@position).id, rep: @rep)
     redirect_to thanks_path
 end
 
 
-def set_phone
-  @rep = "+19195484308" #params[:rep_choice]
-end
 
-def set_message
-  @position= params[:stance]
-end
+private
+
+  def set_phone
+    @phone = "9195484308" #params[:rep_choice].last
+  end
+
+  def set_message
+    @position= params[:stance]
+  end
+  def set_zip
+    @zip= params[:zip]
+  end
+  def set_email
+    @email= params[:email]
+  end
+
+  def set_rep
+    @response = HTTParty.get("http://whoismyrepresentative.com/getall_mems.php?zip=#{params[:zip]}&output=json", format: :plain)
+    @reps = (JSON.parse @response, symbolize_names: true)[:results].uniq
+
+    @reps.each do |rep|
+         if rep[:phone] == params[:rep_choice]
+           @rep = rep[:name]
+         end
+       end
+
+
+  end
+
 
 
 
